1d7c4e296683b3135a4237e944db0483dcd9fcbf
andreepratama27 <andreepratama27@gmail.com>
2023-02-08 17:41:58 +0700

    fix: update header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,17 +11,20 @@
 </script>
 
 <section id="app">
-  <div class="header p-4 px-8 h-56 flex flex-col justify-center leading-6 relative bg-green-500 z-10">
-    <p class="text-white font-bold pb-1">Kirim whatsapp tanpa perlu simpan nomor</p>
-    <p class="text-white font-light">Cepat, mudah, gratis!</p>
+  <div class="header  h-56 flex flex-col justify-center leading-6 relative bg-green-500 z-10">
+    <div class="max-w-2xl w-full mx-auto px-4 py-8 lg:py-8">
+      <p class="text-white font-bold">Kirim whatsapp tanpa perlu simpan nomor <span class="text-3xl">📱</span></p>
+      <p class="text-white font-light">Cepat, mudah, gratis sampe <span class="text-xl text-yellow-300 font-bold">Kiamat</span> <span class="text-3xl">🤯</span></p>
 
-    <div class="h-1 bg-gray-800 w-32 mt-2"></div>
+      <div class="h-2 bg-yellow-300 w-32 mt-2"></div>
 
-    <div class="overlay w-full h-full absolute top-0 left-0 z-0 opacity-20"></div>
+      <div class="overlay w-full h-full absolute top-0 left-0 z-0 opacity-20"></div>
+    </div>
   </div>
 
-  <div class="p-8">
+  <div class="py-8 px-4 lg:py-8 max-w-2xl mx-auto">
     <p>Nomor Whatsapp</p>
+    <div class="w-32 mt-2 mb-4 h-1 bg-gray-800"></div>
 
     <div class="input-wrapper py-4">
       <input type="number" class="w-full bg-white p-4 border-2 rounded-none outline-none border-black" placeholder="Masukkan nomor Whatsapp" bind:value={phoneNumber}>
@@ -34,7 +37,7 @@
 </section>
 
 <footer class='absolute bottom-0 h-14 bg-white border-t w-full items-center justify-center flex'>
-  <p class='text-gray-500 text-xs'>&copy; 2023 Andre - <a href='#'>github.com/andreepratama27</a></p>
+  <p class='text-gray-500 text-xs'>&copy; 2023 Andre - <a href="https://github.com/andreepratama27" target="_blank" rel="noreferrer">github.com/andreepratama27</a></p>
 </footer>
 
 <style type="postcss">
